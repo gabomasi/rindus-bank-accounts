d65a6f267eb44b2118c79a667a0f733c1e7761fd
Gabriel Masi <masigabriel@gmail.com>
2019-08-22 22:29:36 -0300

    Template Improvement

--- bankaccounts/views.py
+++ bankaccounts/views.py
@@ -9,6 +9,7 @@ class DashboardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user'] = self.request.user
+        context['has_permission'] = True
         return context
 
     @method_decorator(login_required)
